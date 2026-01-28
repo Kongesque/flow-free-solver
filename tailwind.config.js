29f1ae3e498fbfd71dce0389916b643ada0ad57c
Kongesque <Kongesque@gmail.com>
2026-01-28 23:41:18 +0800

    update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['"JetBrains Mono"', 'monospace'],
+                mono: ['"JetBrains Mono"', 'monospace'],
+            },
+        },
     },
     plugins: [],
 }
