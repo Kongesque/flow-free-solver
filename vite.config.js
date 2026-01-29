d9e273e168d1d0f88c37ae56f6c027dba3275dc9
Kongesque <kongesque@gmail.com>
2026-01-30 00:01:10 +0700

    fix: update base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/flow-free-solver/',
+    base: process.env.GITHUB_ACTIONS ? '/flow-free-solver/' : '/',
     build: {
         outDir: 'dist',
     },
